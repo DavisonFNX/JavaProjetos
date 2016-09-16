30423eee4a8e73d33d2477011f51bd0395c98d0e
DavisonFNX <davisonfnx@gmail.com>
2016-09-16 17:26:11 -0300

    Exercicios 2

--- com/projetos/cursojava/ListaExecicios2.java
+++ com/projetos/cursojava/ListaExecicios2.java
@@ -31,7 +31,7 @@ public class ListaExecicios2 {
 		}else {
 			System.out.println( "O valor digitado é negativo." );
 		}
-	*/
+	
 		System.out.println( "Digite F ou M para saber o genero: ");
 		char genero = input.nextLine().charAt(0);
 				
@@ -44,8 +44,64 @@ public class ListaExecicios2 {
 		}
 		
 		
+		System.out.print( "Digite uma letra do alfabeto: " );
+		String letra = input.nextLine().toLowerCase();
+		
+		switch (letra){
+			case "a":
+				System.out.println( "A letra digitada é vogal." );
+				break;
+			case "e":
+				System.out.println( "A letra digitada é vogal." );
+				break;
+			case "i":
+				System.out.println( "A letra digitada é vogal." );
+				break;
+			case "o":
+				System.out.println( "A letra digitada é vogal." );
+				break;
+			case "u":
+				System.out.println( "A letra digitada é vogal." );
+				break;
+			default:
+				System.out.println( "A letra digitada é consoante." );
+				
+		}
 		
 		
+		System.out.println( "Digite a primeira nota: ");
+		double nota1 = Double.parseDouble(input.next());
+		
+		System.out.println( "Digite a segunda nota: " );
+		double nota2 = Double.parseDouble(input.next());
+		
+		double mediaFinal = ( nota1 + nota2 ) / 2;
+		
+		if ( mediaFinal == 10) {
+			System.out.println( "Aluno Aprovado com Distinção." );
+			
+		}else if (mediaFinal >= 7) {
+			System.out.println( "Aprovado." );
+			
+		}else {
+			System.out.println( "Reprovado.");
+		}
+		*/
+		
+		System.out.print( "Digite o primeiro número: " );
+		int numero1 = input.nextInt();
+		System.out.print( "Digite o segundo número: " );
+		int numero2 = input.nextInt();
+		System.out.print( "Digite o terceiro número: " );
+		int numero3 = input.nextInt();
+		
+		if ( numero1 > numero2 & numero1 > numero3 ) {
+			System.out.printf( "O %s é o maior número.", numero1 );
+		}else if ( numero2 > numero1 & numero2 >numero3) {
+			System.out.printf( "O %s é o maior número.", numero2 );
+		}else {
+			System.out.printf( "O %s é o maior número.", numero3 );
+		}
 		
 		
 	}
